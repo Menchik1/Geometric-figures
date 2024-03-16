54b9feb340b48c96499608e31bb7e15f38883dc6
Nikola-38 <163429541+Nikola-38@users.noreply.github.com>
2024-03-16 21:36:46 +0700

    Изменил вывод

--- Trapesia.cpp
+++ Trapesia.cpp
@@ -1,18 +1,20 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
+int P, S, Srline;
+int Trapesia(int a, int b, int c, int d, int h) {
 
-void Trapesia(int a, int b, int c, int d, int h) {
-	int P, S, Srline;
 	P = a + b + c + d;
 	S = (a + b) * h / 2;
 	Srline = (a + b) / 2;
-	cout << "Perimeter: " << P << endl;
-	cout << "Area: " << S << endl;
-	cout << "Length of the midline: " << Srline << endl;
+	return 0;
+
 }
 
 int main() {
 	int a = 5, b = 10, c = 6, d = 8, h = 5;
 	Trapesia(a, b, c, d, h);
+	cout << "Perimeter: " << P << endl;
+	cout << "Area: " << S << endl;
+	cout << "Length of the midline: " << Srline << endl;
 	return 0;
 }
